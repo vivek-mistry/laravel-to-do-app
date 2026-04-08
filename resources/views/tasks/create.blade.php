a6bd9e980027c33331ccf19146bb276af1d42e8e
vivek-mistry <vickymistry1818@gmail.com>
2026-04-08 15:57:08 +0530

    - Validate after fillable inputs

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -8,7 +8,7 @@
                 <div class="grid gap-2">
                     <label for="task_description">Description</label>
                     <div class="grid grid-cols-[1fr_180px] gap-2">
-                        <input type="text" id="task_description" name="description" placeholder="describe the task..." tabindex="1" autofocus>
+                        <input type="text" id="task_description" name="description" placeholder="describe the task..." tabindex="1" autofocus value="{{ old('description') }}">
                         <button type="submit" class="btn" tabindex="3">Add</button>
                         @error('description')
                             <p class="text-red-500 text-sm">{{ $message }}</p>
@@ -18,7 +18,7 @@
 
                 <div class="grid gap-2">
                     <label for="task_due_date">Due date</label>
-                    <input type="date" id="task_due_date" name="due_date" tabindex="2">
+                    <input type="date" id="task_due_date" name="due_date" tabindex="2" value="{{ old('due_date') }}">
                     @error('due_date')
                         <p class="text-red-500 text-sm">{{ $message }}</p>
                     @enderror
