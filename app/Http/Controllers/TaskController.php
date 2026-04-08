353e1ff86fd4c70f40884282de7932023f042a26
vivek-mistry <vickymistry1818@gmail.com>
2026-04-08 17:45:25 +0530

    update the things & fixing the codes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -51,31 +51,34 @@ class TaskController extends Controller
             ->with('highlighted_task', $task->id);
     }
 
-    public function inlineUpdate(Request $request, Task $task)
+    public function inlineUpdate(TaskStoreRequest $request, Task $task)
     {
-        $rules = [
-            'name' => 'required|in:description,due_date',
-            'value' => 'nullable|string|max:255',
-            'status' => 'nullable|in:open,overdue',
-        ];
+        // $rules = [
+        //     'name' => 'required|in:description,due_date',
+        //     'value' => 'nullable|string|max:255',
+        //     'status' => 'nullable|in:open,overdue',
+        // ];
 
-        if ($request->input('name') === 'due_date') {
-            $rules['value'] = 'nullable|date|after_or_equal:today';
-        }
+        // if ($request->input('name') === 'due_date') {
+        //     $rules['value'] = 'nullable|date|after_or_equal:today';
+        // }
 
-        $validated = $request->validate($rules);
+        // $validated = $request->validate($rules);
 
-        $update = [];
+        // $update = [];
 
-        if ($validated['name'] === 'description') {
-            $update['description'] = $validated['value'];
-        }
+        // if ($validated['name'] === 'description') {
+        //     $update['description'] = $validated['value'];
+        // }
 
-        if ($validated['name'] === 'due_date') {
-            $update['due_date'] = $validated['value'] ?: null;
-        }
+        // if ($validated['name'] === 'due_date') {
+        //     $update['due_date'] = $validated['value'] ?: null;
+        // }
 
-        $task->update($update);
+        $task->update([
+            'description' => $request->input('description'),
+            'due_date' => $request->input('due_date'),
+        ]);
 
         if ($request->ajax()) {
             return response()->json(['success' => true]);
